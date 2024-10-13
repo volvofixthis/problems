ed4895a2ccc5ca696c651675ce9aaa7eb2f170c6
Konstantin Losev <konstantin.losev@gcore.com>
2024-10-13 23:30:47 +0400

    Added solution for binary tree traversal zigzag

---
+++ leetcode/binary-level-traversal-103/first/main.go
@@ -0,0 +1,38 @@
+package first
+
+import "github.com/volvofixthis/problems/utils"
+
+func zigzagLevelOrder(root *utils.TreeNode) [][]int {
+	m := [][]int{}
+	if root == nil {
+		return m
+	}
+	queue := []*utils.TreeNode{root}
+	reversed := false
+	for len(queue) > 0 {
+		cl := len(queue)
+		line := make([]int, 0, cl)
+		for range cl {
+			current := queue[0]
+			queue = queue[1:]
+			if current.Left != nil {
+				queue = append(queue, current.Left)
+				line = append(line, current.Left.Val)
+			}
+			if current.Right != nil {
+				queue = append(queue, current.Right)
+				line = append(line, current.Right.Val)
+			}
+		}
+		if reversed {
+			for i, j := 0, len(line)-1; i < j; i, j = i+1, j-1 {
+				line[i], line[j] = line[j], line[i]
+			}
+		}
+		if len(line) > 0 {
+			m = append(m, line)
+		}
+		reversed = !reversed
+	}
+	return m
+}
