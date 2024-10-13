dbd4f68f88f2a860d269c3920ba5b2dc7ff92ca7
Konstantin Losev <konstantin.losev@gcore.com>
2024-10-13 22:09:11 +0400

    Added solution for binary tree level traversal II

--- leetcode/binary-level-traversal-107/first/main.go
+++ leetcode/binary-level-traversal-107/first/main.go
@@ -8,23 +8,30 @@ func levelOrder(root *utils.TreeNode) [][]int {
 		return m
 	}
 	queue := []*utils.TreeNode{root}
+	m = append(m, []int{root.Val})
 	for len(queue) > 0 {
 		cl := len(queue)
-		line := []int{}
-		for _, v := range queue {
-			line = append(line, v.Val)
-		}
-		m = append(m, line)
+		line := make([]int, 0, cl)
 		for range cl {
 			current := queue[0]
 			queue = queue[1:]
 			if current.Left != nil {
 				queue = append(queue, current.Left)
+				line = append(line, current.Left.Val)
 			}
 			if current.Right != nil {
 				queue = append(queue, current.Right)
+				line = append(line, current.Right.Val)
+
 			}
 		}
+		if len(line) > 0 {
+			m = append(m, line)
+		}
+	}
+	// Revert array
+	for i, j := 0, len(m)-1; i < j; i, j = i+1, j-1 {
+		m[i], m[j] = m[j], m[i]
 	}
 	return m
 }
